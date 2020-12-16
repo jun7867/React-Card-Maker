ddb942f0d9848c95d0983928666f45512afc889c
남쭌 <jjune095@naver.com>
2020-12-17 03:53:50 +0900

    card-maker init + login_header

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./components/login/login";
+import styles from "./app.module.css";
+function App() {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
